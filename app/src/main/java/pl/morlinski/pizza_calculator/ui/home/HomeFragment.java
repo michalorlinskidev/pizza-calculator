dac51749c9e2ea407a6003d3733fc9b06be3fdcd
Michal Orlinski <michal.orlinski@whirly.pl>
2020-09-21 19:59:58 +0200

    basic pizza calc function

--- app/src/main/java/pl/morlinski/pizza_calculator/ui/home/HomeFragment.java
+++ app/src/main/java/pl/morlinski/pizza_calculator/ui/home/HomeFragment.java
@@ -4,15 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import pl.morlinski.pizza_calculator.R;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
 
 public class HomeFragment extends Fragment {
 
@@ -23,11 +25,29 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        final TextView pizza1Diameter = root.findViewById(R.id.pizza1Diameter);
+        final TextView pizza1Price = root.findViewById(R.id.pizza1Price);
+        final TextView pizza2Diameter = root.findViewById(R.id.pizza2Diameter);
+        final TextView pizza2Price = root.findViewById(R.id.pizza2Price);
+        final TextView result = root.findViewById(R.id.result);
+        final Button pizzaCalc = root.findViewById(R.id.pizzaCalc);
+
+        pizzaCalc.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                double pizza1DiameterValue = Float.parseFloat(pizza1Diameter.getText().toString());
+                double pizza2DiameterValue = Float.parseFloat(pizza2Diameter.getText().toString());
+                double pizza1PriceValue = Float.parseFloat(pizza1Price.getText().toString());
+                double pizza2PriceValue = Float.parseFloat(pizza2Price.getText().toString());
+
+                double pizza1Area = PI * pow(pizza1DiameterValue / 2.0, 2);
+                double pizza2Area = PI * pow(pizza2DiameterValue / 2.0, 2);
+
+                double pizza1PricePer1cm_2 = pizza1PriceValue / pizza1Area;
+                double pizza2PricePer1cm_2 = pizza2PriceValue / pizza2Area;
+
+                result.setText("Cena pizzy (na 1cm^2): " + pizza1PricePer1cm_2 + " vs " + pizza2PricePer1cm_2);
             }
         });
         return root;
